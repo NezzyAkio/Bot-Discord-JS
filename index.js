a3673dea540c734921e960ad62623271d0229e9e
Yuro Akio <nezzypolos465@gmail.com>
2021-10-31 17:21:34 +0000

    Fix Some Typo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js')
 const intents = new Discord.Intents(32767)
 const client = new Discord.Client({ intents })
+const config = require('./config.json')
 
-client.login('ODk0NDUxMzQ2NDcwNzQ0MDc0.YVqMuw.s-zAe7wMrtsUSlQrvzkbGS2m54M')
+client.o("ready", () => console.log(`${client.user.tag} is ready!`))
+
+client.login(config.token)
