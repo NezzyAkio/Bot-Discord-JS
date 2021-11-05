45e7b068216b3906c412a694d96d7ff74b122740
Yuro Akio <87758678+NezzyAkio@users.noreply.github.com>
2021-11-05 14:55:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const intents = new Discord.Intents(32767)
 const client = new Discord.Client({ intents })
 const config = require('./config.json')
 
-client.o("ready", () => console.log(`${client.user.tag} is ready!`))
+client.on("ready", () => console.log(`${client.user.tag} is ready!`))
 
 client.login(config.token)
